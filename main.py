1869a2f3f2cba11c7d104fa79b65bc4c2d48fa5f
liuk <644077730@qq.com>
2023-09-08 17:40:19 +0800

    增加zlib压缩支持

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
                         help='运行模式，task 为执行定时备份，decrypt 为解密，backup为即可执行一次备份，info用于查看已有的包信息')
     parser.add_argument('--disable_enc', '-e', action='count', help='不需要加密')
     parser.add_argument('--disable_gzip', '-g', action='count', help='不需要压缩')
-    parser.add_argument('--use_zlib', '-z', action='count', help='不需要压缩')
+    parser.add_argument('--use_zlib', '-z', action='count', help='使用zlib压缩')
     parser.add_argument('--cache_dir', help='缓存文件路径')
     parser.add_argument('--password', help='加密用的密钥，妥善保存，解密需要用到')
     parser.add_argument('--cloud_path', help='阿里云用于备份的文件路径')
