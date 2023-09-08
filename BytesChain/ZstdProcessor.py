8b9c203318cbfa8ebc3b9f48ef7a788633ec47bf
liuk <644077730@qq.com>
2023-09-08 18:52:15 +0800

    多核心支持

--- BytesChain/ZstdProcessor.py
+++ BytesChain/ZstdProcessor.py
@@ -1,12 +1,14 @@
+import os
+
 import pyzstd
 
 from BytesChain.BytesAbstractProcessor import BytesAbstractProcessor
 
 
 class ZstdProcessor(BytesAbstractProcessor):
-
     def __pack_process__(self, bytes_data: bytes) -> bytes:
-        return pyzstd.compress(bytes_data)
+        option = {pyzstd.CParameter.nbWorkers: os.cpu_count()}
+        return pyzstd.compress(bytes_data,option)
 
     def __unpack_process__(self, bytes_data: bytes) -> bytes:
         return pyzstd.decompress(bytes_data)
